d6ee1f3b6c15669d03858760b2df3b20d55b333a
Joel Scott <jscottusf@gmail.com>
2020-06-22 16:50:24 -0400

    added modal which will be used for put request

--- Credit/ClientApp/src/app/app.module.ts
+++ Credit/ClientApp/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from "./app.component";
 import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 import { EmployeeComponent } from "./employee/employee.component";
 import { EmployeeService } from "../Services/employees.service";
 import { NewEmployeeFormComponent } from "./new-employee-form/new-employee-form.component";
+import { ModalComponent } from "./modal/modal.component";
+
 
 @NgModule({
   declarations: [
@@ -16,12 +19,14 @@ import { NewEmployeeFormComponent } from "./new-employee-form/new-employee-form.
     NavMenuComponent,
     EmployeeComponent,
     NewEmployeeFormComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([{ path: "", component: EmployeeComponent }]),
+    NgbModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent],
--- Credit/ClientApp/src/app/modal/modal.component.ts
+++ Credit/ClientApp/src/app/modal/modal.component.ts
@@ -1,37 +1,32 @@
-import { Component, Input } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <p>Hello, {{name}}!</p>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
-    </div>
-  `
-})
-export class ModalContent {
-  constructor(public activeModal: NgbActiveModal) { }
-}
-
-@Component({
-  selector: 'app-modal',
+  selector: 'modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
 
 export class ModalComponent {
+  closeResult = '';
+
   constructor(private modalService: NgbModal) { }
 
-  open() {
-    const modalRef = this.modalService.open(NgbdModalContent);
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
